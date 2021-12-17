2f1c075d536c1d1bd4c5766a97b3ef4d17bfe050
Gautier <hello.gautiermaire@gmail.com>
2021-12-17 13:57:16 +0100

    update exo5 to add status

--- exo5.js
+++ exo5.js
@@ -2,13 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
 
 	switch(req.url) {
 		case '/welcome':
+			res.writeHead(200, {'Content-Type': 'text/plain'});
 			res.end('hello world');
 			break;
 		default:
+			res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Not found');
 			break;
 	}
