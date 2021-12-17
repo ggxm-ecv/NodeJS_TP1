fa5fc8fc1f8c223b180e82fa07aa722e2203f83d
Gautier <hello.gautiermaire@gmail.com>
2021-12-17 13:52:32 +0100

    fix exo 3 to force status 200

--- exo3.js
+++ exo3.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.statusCode = 200;
   res.writeHead(200, {'Content-Type': 'application/json'});
   var json = JSON.stringify({
     "firstname": "Gautier",
